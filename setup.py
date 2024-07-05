194e78fb6200b68b6700b2dafacc36c5594d3a20
Abhsihek saini <abhisheksaini388@gmail.com>
2024-07-05 23:38:27 +0530

    update_3

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup , find_packages
+
+
+setup(
+
+
+    name="us-visa",
+    version="0.0.0.0",
+    author="Abhishek saini",
+    author_email="abhisheksaini388@gmail.com",
+    packages=find_packages()
+)
